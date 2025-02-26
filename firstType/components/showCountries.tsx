f0a96c94b3d51ef53af26f544bb9388f39716f58
asiman2344 <asimanm024@gmail.com>
2025-02-26 12:32:25 +0400

    ...

--- firstType/components/showCountries.tsx
+++ firstType/components/showCountries.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import './component.css'
-// import axios from 'axios'
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 type Country = {
     name:{
@@ -17,16 +18,45 @@ type Country = {
         png:string,
         svg:string
     },
-
+    population:number,
 }
 
 function showCountries() {
     const [countries, setCountries] = useState<Country[]>([]);
 
+    const handleData = async () => {
+        const res = await axios.get('https://restcountries.com/v3.1/all');
+        setCountries(res.data);
+    }
+
+    useEffect(() => {
+        handleData();  
+    }, []);
+
+    useEffect(() => {
+        if (countries.length > 0) {
+            console.log(countries[0].name.common);
+        }
+    }, [countries]);
+
     return (
+    
         <div className="showCountries">
             <div className="cardWrapper">
-                <h1>asdf</h1>
+                {
+                    countries.map(country=>(
+                        <div className="card border border-gray-200 rounded-b-md" key={country.cca2}>
+                            <div className='flag w-full h-[200px]'>
+                                <img className='w-full h-full object-cover' src={country?.flags.png} alt="" />
+                            </div>
+                            <div className='countryInfo'>
+                                <div className='countryName'><h1 className='text-2xl font-medium'>{country?.name?.common}</h1></div>
+                                <div className='countryPop'><span className='text-16px font-medium'>Population: {country?.population}</span></div>
+                                <div className='countryReg text-16px font-medium'>Region: {country?.region}</div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
